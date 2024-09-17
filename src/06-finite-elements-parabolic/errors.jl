365e28bbdca537e48f6eb6afc77e6756017df39a
Shikiyo <shikiyo@archjk>
2024-09-17 12:02:58 -0300

    parabolic: implement errors.jl

--- src/06-finite-elements-parabolic/errors.jl
+++ src/06-finite-elements-parabolic/errors.jl
@@ -3,7 +3,8 @@ include("../common.jl")
 
 import .Common: calc_error, n_points_from_to
 
-using .FiniteElements: finite_elements, bacarmo_example
+using .FiniteElements: fe_setup, fe_step
+using .FiniteElements: bacarmo_example
 
 using Plots
 using LaTeXStrings
@@ -14,25 +15,45 @@ exact, ex = bacarmo_example(0x0)
 min_max = 2:10
 
 N_es = (1 .<< min_max)
-hs = (ex.x_end - ex.x_begin) ./ N_es
-cs = finite_elements.(ex, hs, N_es)
+N_ts = N_es
+hs = 1 ./ N_es
+taus = hs
 
-errs = Common.gauss_error.(exact, cs, hs,
-    x_begin=ex.x_begin,
-    x_end=ex.x_end,
-)
+ABcEs = fe_setup.(ex, taus, hs, N_es)
 
-display(DataFrame(N=min_max, h=hs, error=errs))
+tss = (tau -> 0:tau:ex.T).(taus)
 
-p = plot(hs, hs .* hs,
-    label=L"$O(h^2)$",
+errss = ((ABcE, args...) ->
+((A, B, c_init, EQoLG, h, tau, ts, N_e, N_t) -> begin
+    c0 = c_init
+    errs = fill(0.0, (N_t + 1,))
+
+    errs[1] = Common.gauss_error(x -> exact(x, 0.0), c0, h)
+
+    for i in 1:N_t
+        t0 = ts[i]
+        t1 = ts[i+1]
+        c = fe_step(ex, A, B, c0, t0, tau, h, N_e, EQoLG)
+        c0 = c
+
+        errs[i+1] = Common.gauss_error(x -> exact(x, t1), c, h)
+    end
+    maximum(errs)
+end)(ABcE..., args...)).(ABcEs, hs, taus, tss, N_es, N_ts)
+
+display(DataFrame(N=min_max, h=hs, error=errss))
+
+p = plot(
     yscale=:log2,
     xscale=:log2,
     xlabel=L"log_2(h)",
     ylabel=L"log_2",
     legend=:topleft,
 )
-plot!(p, hs, errs,
+plot!(p, hs, hs .* hs,
+    label=L"$O(h^2)$",
+)
+plot!(p, hs, errss,
     label="finite element errors",
     markershape=:circle,
 )
