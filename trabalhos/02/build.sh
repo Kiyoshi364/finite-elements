1669c5b7a3dcc1ae41ad1a66d7b2b95ab79ac820
Shikiyo <shikiyo@archjk>
2024-10-03 13:13:49 -0300

    Start Trabalho 02

---
+++ trabalhos/02/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -xe
+
+DEFAULT_FILE="main"
+file="${1:-$DEFAULT_FILE}"
+
+mkdir -p folder-build
+pushd folder-build
+pdflatex ../"$file".tex </dev/null
+if [[ "$file" = "$DEFAULT_FILE" ]] then
+  pdftotext "$file".pdf "$file".txt
+  mv "$DEFAULT_FILE".pdf ../o-trabalho-02.pdf
+  cat "$file".txt \
+    | aspell -d pt_BR \
+    --add-wordlists=../wordlist.txt \
+    --ignore 2 \
+    list \
+
+fi
+
+popd
