7c24517d87fc637c60584941924dadb2c5bc1cb3
Shikiyo <shikiyo@archjk>
2024-10-12 17:21:07 -0300

    Trabalho02: add missing pdf structure

--- trabalhos/02/build.sh
+++ trabalhos/02/build.sh
@@ -8,6 +8,7 @@ file="${1:-$DEFAULT_FILE}"
 mkdir -p folder-build
 pushd folder-build
 pdflatex ../"$file".tex </dev/null
+pdflatex ../"$file".tex </dev/null
 if [[ "$file" = "$DEFAULT_FILE" ]] then
   pdftotext "$file".pdf "$file".txt
   mv "$DEFAULT_FILE".pdf ../o-trabalho-02.pdf
