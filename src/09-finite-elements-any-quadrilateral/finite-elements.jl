60014c747de551c4a9f338a65ff63a2883c6107e
Shikiyo <shikiyo@archjk>
2024-11-10 14:53:55 -0300

    any quad: add build_mat_2d

--- src/09-finite-elements-any-quadrilateral/finite-elements.jl
+++ src/09-finite-elements-any-quadrilateral/finite-elements.jl
@@ -17,7 +17,9 @@ export Example, example
 
 # For tests
 export build_LG, build_EQ
+export dx2xis_f
 export build_small_vec_2d, build_vec_2d
+export build_small_mat_2d, build_mat_2d
 
 const phi = [
     (xi -> (1 - xi[1])*(1 - xi[2]) / 4),
@@ -40,17 +42,29 @@ const phi_deriv = [
     ]
 ]
 
+const sdim = 2
+const app = (f, xs...) -> f(xs...)
+const dx2xis_f = (ps, Xe, Ye) -> [
+    [
+        [
+            dot([Xe, Ye][a], app.(phi_deriv[b], ([pi, pj],)))
+            for a in 1:sdim
+            for b in 1:sdim
+        ]
+        for pj in ps
+    ]
+    for pi in ps
+]
+
 const hipe_x2xi = (hi, pe) -> (xi1, xi2) -> [
     (hi[1] * ((1 + xi1) / 2) + pe[1]),
     (hi[2] * ((1 + xi2) / 2) + pe[2]),
 ]
 
-function build_small_mat_2d(alpha, beta, hi;
-    gauss_n = 2,
+function build_small_mat_2d(alpha, beta,
+    dx2xis, ws, ps, gauss_n,
 )
     dim = 4
-
-    ws, ps = gauss_quadrature_table[gauss_n]
 
     K_alpha1 = fill(0.0, (dim,dim))
     for i in 1:dim
@@ -59,13 +73,21 @@ function build_small_mat_2d(alpha, beta, hi;
                 p1 = ps[g_i]
                 for g_j in 1:gauss_n
                     p2 = ps[g_j]
-                    K_alpha1[i,j] += ws[g_i] * ws[g_j] *
-                        (phi_deriv[1][j](p1, p2)*phi_deriv[1][i](p1, p2))
+                    _J = dx2xis[g_i][g_j]
+                    J = 1.0 / ((_J[1] * _J[4]) - (_J[2] * _J[3]))
+                    H_1 = (_J[4]*_J[4] + _J[2]*_J[2])
+                    H_2 = - (_J[4]*_J[3] + _J[2]*_J[1])
+                    K_alpha1[i,j] += J * ws[g_i] * ws[g_j] * (
+                        phi_deriv[1][j]([p1, p2]) * (
+                            (H_1 * phi_deriv[1][i]([p1, p2]))
+                            + (H_2 * phi_deriv[2][i]([p1, p2]))
+                        )
+                    )
                 end
             end
         end
     end
-    K_alpha1 *= alpha * hi[2] / hi[1]
+    K_alpha1 *= alpha
 
     K_alpha2 = fill(0.0, (dim,dim))
     for i in 1:dim
@@ -74,13 +96,21 @@ function build_small_mat_2d(alpha, beta, hi;
                 p1 = ps[g_i]
                 for g_j in 1:gauss_n
                     p2 = ps[g_j]
-                    K_alpha2[i,j] += ws[g_i] * ws[g_j] *
-                        (phi_deriv[2][j](p1, p2)*phi_deriv[2][i](p1, p2))
+                    _J = dx2xis[g_i][g_j]
+                    J = 1.0 / ((_J[1] * _J[4]) - (_J[2] * _J[3]))
+                    H_1 = - (_J[4]*_J[3] + _J[2]*_J[1])
+                    H_2 = (_J[3]*_J[3] + _J[1]*_J[1])
+                    K_alpha2[i,j] += J * ws[g_i] * ws[g_j] * (
+                        phi_deriv[2][j]([p1, p2]) * (
+                            (H_1 * phi_deriv[1][i]([p1, p2]))
+                            + (H_2 * phi_deriv[2][i]([p1, p2]))
+                        )
+                    )
                 end
             end
         end
     end
-    K_alpha2 *= alpha * hi[1] / hi[2]
+    K_alpha2 *= alpha
 
     K_beta = fill(0.0, (dim,dim))
     for i in 1:dim
@@ -89,30 +119,41 @@ function build_small_mat_2d(alpha, beta, hi;
                 p1 = ps[g_i]
                 for g_j in 1:gauss_n
                     p2 = ps[g_j]
-                    K_beta[i,j] += ws[g_i] * ws[g_j] *
-                        (phi[j](p1, p2)*phi[i](p1, p2))
+                    _J = dx2xis[g_i][g_j]
+                    J = (_J[1] * _J[4]) - (_J[2] * _J[3])
+                    K_beta[i,j] += J * ws[g_i] * ws[g_j] *
+                        (phi[j]([p1, p2])*phi[i]([p1, p2]))
                 end
             end
         end
     end
-    K_beta *= beta * hi[1] * hi[2] / 4
+    K_beta *= beta
 
     K_alpha1 + K_alpha2 + K_beta
 end
 
-function build_mat_2d(alpha, beta, hi, N_e, EQoLG, m;
-    gauss_n = 2
+function build_mat_2d(alpha, beta, X, Y, N_e, LG, EQoLG, m;
+    gauss_n = 5
 )
-    K_e = build_small_mat_2d(alpha, beta, hi,
-        gauss_n=gauss_n,
-    )
+    local sdim = 2
+    local ws, ps = gauss_quadrature_table[gauss_n]
 
-    K = spzeros((m+1, m+1))
+    local K = spzeros((m+1, m+1))
     for e in 1:N_e
-        _1 = EQoLG[1, e]
-        _2 = EQoLG[2, e]
-        _3 = EQoLG[3, e]
-        _4 = EQoLG[4, e]
+        local LGe = LG[:, e]
+        local Xe = X[LGe]
+        local Ye = Y[LGe]
+
+        local dx2xis = dx2xis_f(ps, Xe, Ye)
+
+        local K_e = build_small_mat_2d(alpha, beta,
+            dx2xis, ws, ps, gauss_n,
+        )
+
+        local _1 = EQoLG[1, e]
+        local _2 = EQoLG[2, e]
+        local _3 = EQoLG[3, e]
+        local _4 = EQoLG[4, e]
         K[_1,_1] += K_e[1,1]
         K[_1,_2] += K_e[1,2]
         K[_1,_3] += K_e[1,3]
@@ -160,11 +201,7 @@ end
 function build_vec_2d(f, X, Y, N_e, LG, EQoLG, m;
     gauss_n = 5,
 )
-    local sdim = 2
-    local dim = 4
     local ws, ps = gauss_quadrature_table[gauss_n]
-
-    local app = (f, xs...) -> f(xs...)
 
     local F = fill(0.0, (m+1,))
     for e in 1:N_e
@@ -180,17 +217,7 @@ function build_vec_2d(f, X, Y, N_e, LG, EQoLG, m;
             for pi in ps
         ]
 
-        local dx2xis = [
-            [
-                [
-                    dot([Xe, Ye][a], app.(phi_deriv[b], ([pi, pj],)))
-                    for a in 1:sdim
-                    for b in 1:sdim
-                ]
-                for pj in ps
-            ]
-            for pi in ps
-        ]
+        local dx2xis = dx2xis_f(ps, Xe, Ye)
 
         local F_e = build_small_vec_2d(f, x2xis,
             dx2xis, ws, ps, gauss_n
--- src/09-finite-elements-any-quadrilateral/tests.jl
+++ src/09-finite-elements-any-quadrilateral/tests.jl
@@ -4,7 +4,9 @@ using .FiniteElements: gauss_quadrature_table
 using .FiniteElements: phi, phi_deriv
 
 using .FiniteElements: build_LG, build_EQ
+using .FiniteElements: dx2xis_f
 using .FiniteElements: build_small_vec_2d, build_vec_2d
+using .FiniteElements: build_small_mat_2d, build_mat_2d
 
 using LinearAlgebra: dot, norm
 
@@ -26,6 +28,7 @@ function run_tests(name, func, max_i :: UInt8;
         ) : error("Invalid bench_strategy")
         func(i, bench=bench)
     end
+    println()
 end
 
 const default_tol = 1e-12
@@ -33,16 +36,14 @@ function test_check(i :: UInt8, expected, actual;
     tol=default_tol
 )
     local err = norm((expected - actual)[begin:end])
+    println("test $i: err is $err, tol is $tol")
     if err >= tol
         println("==============================")
-        println("test $i: err is $err, tol is $tol")
         println("expected:")
         display(expected)
         println("actual:")
         display(actual)
         println("==============================")
-    else
-        println("test $i: err is $err, tol is $tol")
     end
 end
 
@@ -54,8 +55,6 @@ function test_small_vec_2d(i :: UInt8;
     local ws, ps = gauss_quadrature_table[gauss_n]
 
     local app = (f, xs...) -> f(xs...)
-    local sdim = 2
-    local dim = 4
 
     local hi = [0.25, 0.25]
     local Xe, Ye = (i <= 1) ? (
@@ -82,17 +81,7 @@ function test_small_vec_2d(i :: UInt8;
         for pi in ps
     ]
 
-    local dx2xis = (Xe, Ye) -> [
-        [
-            [
-                dot([Xe, Ye][a], app.(phi_deriv[b], ([pi, pj],)))
-                for a in 1:sdim
-                for b in 1:sdim
-            ]
-            for pj in ps
-        ]
-        for pi in ps
-    ]
+    local dx2xis = dx2xis_f(ps, Xe, Ye)
 
     local f = (i == 0) ? (
         x -> 4.0 / foldl(*, hi)
@@ -104,10 +93,10 @@ function test_small_vec_2d(i :: UInt8;
 
     local ans = (bench) ? (@btime build_small_vec_2d(
         $f, $(x2xis(Xe, Ye)),
-        $(dx2xis(Xe, Ye)), $ws, $ps, $gauss_n
+        $dx2xis, $ws, $ps, $gauss_n
     )) : (build_small_vec_2d(
         f, x2xis(Xe, Ye),
-        dx2xis(Xe, Ye), ws, ps, gauss_n
+        dx2xis, ws, ps, gauss_n
     ))
 
     test_check(i, expected, ans)
@@ -188,5 +177,134 @@ function test_vec_2d(i :: UInt8; bench=true)
     ans
 end
 
+const test_small_mat_2d_max = 0x02
+function test_small_mat_2d(i :: UInt8;
+    bench=true
+)
+    local gauss_n = 5
+    local ws, ps = gauss_quadrature_table[gauss_n]
+
+    local alpha, beta = (i == 0) ? (
+        6.0, 0.0
+    ) : (i == 1) ? (
+        0.0, 576.0
+    ) : (i == 2) ? (
+        1.0, 1.0
+    ) : error("Index out of bounds")
+
+    local hi = [0.25, 0.25]
+    local Xe, Ye = (i <= 1) ? (
+        [ 0.0, hi[1], hi[1], 0.0 ],
+        [ 0.0, 0.0, hi[2], hi[2] ],
+    ) : (i == 2) ? (
+        [ 0.0, 2.0, 3.0, 1.0 ],
+        [ 0.0, 0.0, 1.0, 1.0 ],
+    ) : error("Index out of bounds")
+
+    local expected = (i == 0) ? ( [
+        (4.0) (-1.0) (-2.0) (-1.0);
+        (-1.0) (4.0) (-1.0) (-2.0);
+        (-2.0) (-1.0) (4.0) (-1.0);
+        (-1.0) (-2.0) (-1.0) (4.0);
+    ] ) : (i == 1) ? ( [
+        4.0 2.0 1.0 2.0;
+        2.0 4.0 2.0 1.0;
+        1.0 2.0 4.0 2.0;
+        2.0 1.0 2.0 4.0;
+    ] ) : (i == 2) ? ( [
+        (0.7222222222222222) (0.1111111111111111) (0.0555555555555555) (-0.3888888888888888);
+        (0.1111111111111111) (1.7222222222222222) (-0.3888888888888888) (-0.9444444444444444);
+        (0.0555555555555555) (-0.3888888888888888) (0.7222222222222222) (0.1111111111111111);
+        (-0.3888888888888888) (-0.9444444444444444) (0.1111111111111111) (1.7222222222222222);
+    ] ) : error("Index out of bounds")
+
+    local dx2xis = dx2xis_f(ps, Xe, Ye)
+
+    local ans = (bench) ? (@btime build_small_mat_2d(
+        $alpha, $beta,
+        $dx2xis, $ws, $ps, $gauss_n
+    )) : (build_small_mat_2d(
+        alpha, beta,
+        dx2xis, ws, ps, gauss_n
+    ))
+
+    test_check(i, expected, ans)
+
+    ans
+end
+
+const test_mat_2d_max = 0x01
+function test_mat_2d(i :: UInt8; bench=true)
+    local gauss_n = 5
+
+    local alpha, beta = 1.0, 1.0
+
+    local Ni = [4, 3]
+    local N_e = foldl(*, Ni)
+    local hi = 1 ./ Ni
+
+    local LG = build_LG(Ni)
+    local m, EQ = build_EQ(Ni)
+    local EQoLG = EQ[LG]
+
+    local X, Y = (i == 0) ? (
+        repeat(0.0:(hi[1]):1.0, Ni[2]+1),
+        cat(
+            ((x->repeat(x:x, Ni[1]+1)).(0.0:(hi[2]):1.0))...,
+            dims=1
+        ),
+    ) : (i == 1) ? ( [
+        0.0, 0.25, 0.5, 0.75, 1.0,
+        0.0, 0.266168, 0.493792, 0.747176, 1.0,
+        0.0, 0.275391, 0.521668, 0.708237, 1.0,
+        0.0, 0.25, 0.5, 0.75, 1.0,
+    ], [
+        0.0, 0.0, 0.0, 0.0, 0.0,
+        0.333333, 0.352246, 0.36139, 0.326172, 0.333333,
+        0.666667, 0.633228, 0.693524, 0.689905, 0.666667,
+        1.0, 1.0, 1.0, 1.0, 1.0,
+    ] ) : error("Index out of bounds")
+
+    local expected = (i == 0) ? ( [
+          2.81481    -0.62963     0.0        -0.0462963  -0.344907    0.0;
+         -0.62963     2.81481    -0.62963    -0.344907   -0.0462963  -0.344907;
+          0.0        -0.62963     2.81481     0.0        -0.344907   -0.0462963;
+         -0.0462963  -0.344907    0.0         2.81481    -0.62963     0.0;
+         -0.344907   -0.0462963  -0.344907   -0.62963     2.81481    -0.62963;
+          0.0        -0.344907   -0.0462963   0.0        -0.62963     2.81481;
+    ] ) : (i == 1) ? ( [
+          2.78503   -0.710679    0.0        -0.203308  -0.246537    0.0;
+         -0.710679   2.93963    -0.692196   -0.453569   0.0280001  -0.421416;
+          0.0       -0.692196    2.86423     0.0       -0.321263    0.0453884;
+         -0.203308  -0.453569    0.0         2.82204   -0.608672    0.0;
+         -0.246537   0.0280001  -0.321263   -0.608672   2.87005    -0.885907;
+          0.0       -0.421416    0.0453884   0.0       -0.885907    2.91071;
+    ] ) : error("Index out of bounds")
+
+    local f = (i == 0) ? (
+        x -> 4.0 / foldl(*, hi)
+    ) : (i == 1) ? (
+        x -> 16. * 9.0 * foldl(*, x) / foldl(*, hi) / foldl(*, hi)
+    ) : (i == 2) ? (
+        x -> foldl(+, x)
+    ) : error("Index out of bounds")
+
+    local ans = (bench) ? (@btime build_mat_2d(
+        $alpha, $beta, $X, $Y, $N_e, $LG, $EQoLG, $m,
+        gauss_n=$gauss_n
+    )) : (build_mat_2d(
+        alpha, beta, X, Y, N_e, LG, EQoLG, m,
+        gauss_n=gauss_n
+    ))
+
+    test_check(i, expected, ans,
+        tol=(i == 1) ? 1e-2 : 1e-4
+    )
+
+    ans
+end
+
 run_tests("test_small_vec_2d", test_small_vec_2d, test_small_vec_2d_max, bench_strategy=:all)
 run_tests("test_vec_2d", test_vec_2d, test_vec_2d_max, bench_strategy=:last)
+run_tests("test_small_mat_2d", test_small_mat_2d, test_small_mat_2d_max, bench_strategy=:all)
+run_tests("test_mat_2d", test_mat_2d, test_mat_2d_max, bench_strategy=:last)
