93238c53cd61d689a7afeb6f9e0f9626f041aa08
Shikiyo <shikiyo@archjk>
2024-12-11 16:56:06 -0300

    Any quads: add single-code graphs

--- src/09-finite-elements-any-quadrilateral/compile_benchmarks.jl
+++ src/09-finite-elements-any-quadrilateral/compile_benchmarks.jl
@@ -5,7 +5,7 @@ import BenchmarkTools as BT
 using Plots: plot, plot!, savefig
 
 const names = [
-    :baseline, :ref, :iter, :iter_ref,
+    :baseline, :ref, :iter, :iter_ref, :bacarmo
 ]
 const markershapes = [
     :circle,
@@ -19,17 +19,19 @@ const name_prettyname = (;
     ref = "Ref",
     iter = "Iter",
     iter_ref = "Iter and Ref",
+    bacarmo = "Bruno Carmo",
 )
 
-function make_graph(
+function make_group_graph(
     group :: BenchmarkGroup;
     label :: String = "label",
 )
+    local scale = :log10
     local p = plot(
-        xscale=:log2,
-        xlabel="N1 (log2)",
-        # yscale=:log10,
-        ylabel="$(label)",
+        xscale=scale,
+        xlabel="N ($(String(scale)))",
+        yscale=scale,
+        ylabel="$(label) ($(String(scale)))",
         legend=:topleft,
     )
     for (i, name) in enumerate(names)
@@ -41,7 +43,7 @@ function make_graph(
         keys_str = keys_str[perm]
         keys_int = keys_int[perm]
         local values_ = getindex.((subgroup,), keys_str)
-        plot!(p, keys_int, values_,
+        plot!(p, keys_int .* keys_int, values_,
             label=pretty_name,
             markershape=markershapes[i],
         )
@@ -49,19 +51,61 @@ function make_graph(
     p
 end
 
-const bench = load("results.json")[1]
+function make_single_graph(
+    name :: String,
+    group :: BenchmarkGroup,
+)
+    local label_func = [
+        ("minimum time (ns)", (x -> BT.time(BT.minimum(x)))),
+        ("allocs", (x -> BT.allocs(BT.minimum(x)))),
+        ("memory (bytes)", (x -> BT.memory(BT.minimum(x)))),
+    ]
+    local scale = :log10
+    local p = plot(
+        xscale=scale,
+        xlabel="N elements ($(String(scale)))",
+        yscale=scale,
+        ylabel="($(String(scale)))",
+        legend=:topleft,
+    )
+    local keys_str = [ k for k in keys(group) ]
+    local keys_int = parse.(Int64, keys_str)
+    local perm = sortperm(keys_int)
+    keys_str = keys_str[perm]
+    keys_int = keys_int[perm]
+    local trials = getindex.((group,), keys_str)
+    for (i, (label, func)) in enumerate(label_func)
+        local values_ = func.(trials)
+        plot!(p, keys_int .* keys_int, values_,
+            label=label,
+            markershape=markershapes[i],
+        )
+    end
+    p
+end
 
-const file_label_func = [
+const bench = load("results.json")[1]
+const out_dir = "./out/"
+const ext = "pdf"
+
+const groups_file_label_func = [
     ("min_time", "minimum time (ns)", (x -> BT.time(BT.minimum(x)))),
     ("min_alloc", "allocs", (x -> BT.allocs(BT.minimum(x)))),
     ("min_memory", "memory (bytes)", (x -> BT.memory(BT.minimum(x)))),
 ]
-const ext = "pdf"
 
-for (file, label, f) in file_label_func
-    local p = make_graph(
+for (file, label, f) in groups_file_label_func
+    local p = make_group_graph(
         f(bench),
         label=label
     )
-    savefig(p, "$(file).$(ext)")
+    savefig(p, "$(out_dir)$(file).$(ext)")
+end
+
+for (name, single_bench) in bench
+    local p = make_single_graph(
+        name,
+        single_bench,
+    )
+    savefig(p, "$(out_dir)$(name).$(ext)")
 end
