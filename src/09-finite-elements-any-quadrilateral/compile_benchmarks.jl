c6ff8c180c5511712917736539282627aa794f5e
Shikiyo <shikiyo@archjk>
2024-12-13 15:23:43 -0300

    Any quads: add local K and F to benchmarks

--- src/09-finite-elements-any-quadrilateral/compile_benchmarks.jl
+++ src/09-finite-elements-any-quadrilateral/compile_benchmarks.jl
@@ -4,6 +4,20 @@ import BenchmarkTools as BT
 
 using Plots: plot, plot!, savefig
 
+const small_names = [
+    :base, :base_precalc, :ref, :ref_precalc, :bacarmo
+]
+const smallname_prettyname = (;
+    base = "Baseline",
+    base_precalc = "Baseline with precalculation",
+    ref = "Ref",
+    ref_precalc = "Ref with precalculation",
+    bacarmo = "Bruno Carmo",
+)
+const small_tests_names = [
+    (:build_small_vec, "smallvec"),
+    (:build_small_mat, "smallmat"),
+]
 const names = [
     :baseline, :ref, :iter, :iter_ref, :bacarmo
 ]
@@ -26,6 +40,32 @@ const name_prettyname = (;
     iter_ref = "Iter and Ref",
     bacarmo = "Bruno Carmo",
 )
+
+function make_small_csv(
+    group :: BenchmarkGroup,
+    io :: IO,
+)
+    local colunms_funcs = [
+        ("min_time (ns)", (x -> BT.time(BT.minimum(x)))),
+        ("allocs", (x -> BT.allocs(BT.minimum(x)))),
+        ("memory (bytes)", (x -> BT.memory(BT.minimum(x)))),
+    ]
+    write(io, "Name")
+    for (col_name, func) in colunms_funcs
+        write(io, ",")
+        write(io, col_name)
+    end
+    write(io, "\n")
+    for small_name in small_names
+        local trial = group[String(small_name)]
+        write(io, smallname_prettyname[small_name])
+        for (col_name, func) in colunms_funcs
+            write(io, ",")
+            write(io, string(func(trial)))
+        end
+        write(io, "\n")
+    end
+end
 
 function make_group_graph(
     group :: BenchmarkGroup;
@@ -100,6 +140,20 @@ const groups_file_label_func = [
     ("min_alloc", "allocs", (x -> BT.allocs(BT.minimum(x)))),
     ("min_memory", "memory (bytes)", (x -> BT.memory(BT.minimum(x)))),
 ]
+
+for (small_test, small_test_name) in small_tests_names
+    local small_test_bench = bench[small_test]
+    if 0 < length(small_test_bench)
+        local file = open(
+            "$(out_dir)$(small_test_name).csv",
+            lock = false,
+            create = true,
+            write = true,
+        )
+        make_small_csv(small_test_bench, file)
+        close(file)
+    end
+end
 
 for (test, test_name) in tests_names
     local test_bench = bench[test]
--- src/09-finite-elements-any-quadrilateral/make_benchmarks.jl
+++ src/09-finite-elements-any-quadrilateral/make_benchmarks.jl
@@ -5,7 +5,10 @@ using .FiniteElements: generate_space
 
 using .FiniteElements: build_LG, build_EQ
 using .FiniteElements: phis_f, phi_derivs_f
+using .FiniteElements: x2xis_f, dx2xis_f
 
+using .FiniteElements: build_small_vec_2d, build_small_vec_2d_ref!
+using .FiniteElements: build_small_mat_2d, build_small_mat_2d_ref!
 
 using .FiniteElements: build_vec_2d, build_vec_2d_ref
 using .FiniteElements: build_vec_2d_iter, build_vec_2d_iterref
@@ -17,6 +20,7 @@ using .FiniteElements: build_vec_mat_2d, build_vec_mat_2d_ref
 using .FiniteElements: build_vec_mat_2d_iter, build_vec_mat_2d_iterref
 
 include("bacarmo.jl")
+using .Bacarmo: monta_Kᵉ_quadrilatero!, monta_Fᵉ_quadrilatero!
 using .Bacarmo: monta_K_quadrilatero, monta_F_quadrilatero
 
 using BenchmarkTools: @benchmarkable, BenchmarkGroup
@@ -28,6 +32,22 @@ const names = [
 const tests = [
     :build_vec, :build_mat, :build_both,
 ]
+const small_tests_groups = (;
+    build_small_vec  = [
+        (:base        , build_small_vec_2d),
+        (:base_precalc, build_small_vec_2d),
+        (:ref         , build_small_vec_2d_ref!),
+        (:ref_precalc , build_small_vec_2d_ref!),
+        (:bacarmo     , monta_Fᵉ_quadrilatero!),
+    ],
+    build_small_mat  = [
+        (:base        , build_small_mat_2d),
+        (:base_precalc, build_small_mat_2d),
+        (:ref         , build_small_mat_2d_ref!),
+        (:ref_precalc , build_small_mat_2d_ref!),
+        (:bacarmo     , monta_Kᵉ_quadrilatero!),
+    ],
+)
 const tests_groups = (;
     build_vec  = [
         (:baseline, build_vec_2d),
@@ -49,9 +69,11 @@ const tests_groups = (;
     ],
 )
 const tests_tags = [
-    (:build_vec,  ["build_vec"]),
-    (:build_mat,  ["build_mat"]),
-    (:build_both, ["build_both"]),
+    (:build_small_vec,  ["build_small_vec"]),
+    (:build_small_mat,  ["build_small_mat"]),
+    (:build_vec,        ["build_vec"]),
+    (:build_mat,        ["build_mat"]),
+    (:build_both,       ["build_both"]),
 ]
 
 const names_fullfuncs = (;
@@ -71,15 +93,163 @@ const names_tags = (;
 suite = BenchmarkGroup()
 for (test, test_tags) in tests_tags
     suite[test] = BenchmarkGroup(test_tags)
-    for (impl_name, func) in tests_groups[test]
-        suite[test][impl_name] = BenchmarkGroup(names_tags[impl_name])
+    if test in keys(tests_groups)
+        for (impl_name, func) in tests_groups[test]
+            suite[test][impl_name] = BenchmarkGroup(names_tags[impl_name])
+        end
     end
 end
 
 const f = (x...) -> foldl(+, x)
 const alpha, beta = 1.0, 1.0
 
-const min_max = 2:7
+const min_max = 2:5
+
+begin
+    local Ni = [1, 1]
+    local hi = 1.0 ./ Ni
+
+    local Xe, Ye = ([
+        0.0, 1.0, 1.0, 0.0
+    ], [
+        0.0, 0.0, 1.0, 1.0
+    ])
+
+    local gauss_n = 5
+    local ws, ps = gauss_quadrature_table[gauss_n]
+
+    local phis = phis_f(ps)
+    local phi_derivs = phi_derivs_f(ps)
+
+    local x2xis = x2xis_f(phis, Xe, Ye)
+    local dx2xis = dx2xis_f(phi_derivs, Xe, Ye)
+
+    for (name, func) in small_tests_groups[:build_small_vec]
+        local sub_suite = suite[:build_small_vec]
+        if name == :base
+            sub_suite[name] = @benchmarkable begin
+                local x2xis = x2xis_f($phis, $Xe, $Ye)
+                local dx2xis = dx2xis_f($phi_derivs, $Xe, $Ye)
+
+                $(func)(
+                    $f,
+                    x2xis, dx2xis,
+                    $phis, $ws, $gauss_n
+                )
+            end
+        elseif name == :base_precalc
+            sub_suite[name] = @benchmarkable begin
+                $(func)(
+                    $f,
+                    $x2xis, $dx2xis,
+                    $phis, $ws, $gauss_n
+                )
+            end
+        elseif name == :ref
+            local ref_out = Ref(fill(0.0, (4,)))
+            sub_suite[name] = @benchmarkable begin
+                local x2xis = x2xis_f($phis, $Xe, $Ye)
+                local dx2xis = dx2xis_f($phi_derivs, $Xe, $Ye)
+
+                $(func)(
+                    $ref_out,
+                    $f,
+                    x2xis, dx2xis,
+                    $phis, $ws, $gauss_n
+                )
+                $ref_out[]
+            end
+        elseif name == :ref_precalc
+            local ref_out = Ref(fill(0.0, (4,)))
+            sub_suite[name] = @benchmarkable begin
+                $(func)(
+                    $ref_out,
+                    $f,
+                    $x2xis, $dx2xis,
+                    $phis, $ws, $gauss_n
+                )
+                $ref_out[]
+            end
+        elseif name == :bacarmo
+            local mat = fill(0.0, (4,))
+            sub_suite[name] = @benchmarkable begin
+                $(func)(
+                    $mat,
+                    $f,
+                    $Xe, $Ye,
+                    $ps, $ws
+                )
+                $mat
+            end
+        else
+            error("Unknown symbol $impl")
+        end
+    end
+
+    for (name, func) in small_tests_groups[:build_small_mat]
+        local sub_suite = suite[:build_small_mat]
+        if name == :base
+            sub_suite[name] = @benchmarkable begin
+                local dx2xis = dx2xis_f($phi_derivs, $Xe, $Ye)
+
+                $(func)(
+                    $alpha, $beta,
+                    dx2xis,
+                    $phis, $phi_derivs,
+                    $ws, $gauss_n,
+                )
+            end
+        elseif name == :base_precalc
+            sub_suite[name] = @benchmarkable begin
+                $(func)(
+                    $alpha, $beta,
+                    $dx2xis,
+                    $phis, $phi_derivs,
+                    $ws, $gauss_n,
+                )
+            end
+        elseif name == :ref
+            local ref_out = Ref(fill(0.0, (4,4)))
+            sub_suite[name] = @benchmarkable begin
+                local dx2xis = dx2xis_f($phi_derivs, $Xe, $Ye)
+
+                $(func)(
+                    $ref_out,
+                    $alpha, $beta,
+                    dx2xis,
+                    $phis, $phi_derivs,
+                    $ws, $gauss_n,
+                )
+                $ref_out
+            end
+        elseif name == :ref_precalc
+            local ref_out = Ref(fill(0.0, (4,4)))
+            sub_suite[name] = @benchmarkable begin
+                $(func)(
+                    $ref_out,
+                    $alpha, $beta,
+                    $dx2xis,
+                    $phis, $phi_derivs,
+                    $ws, $gauss_n,
+                )
+                $ref_out
+            end
+        elseif name == :bacarmo
+            local mat = fill(0.0, (4,4))
+            sub_suite[name] = @benchmarkable begin
+                $(func)(
+                    $mat,
+                    $alpha, $beta,
+                    $Xe, $Ye,
+                    $ps, $ws,
+                )
+                $mat
+            end
+        else
+            error("Unknown symbol $impl")
+        end
+    end
+end
 
 for i in min_max
     local pow = 1 << i
